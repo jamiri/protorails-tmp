aad53d02c79216ec1f97a4f80d169ded52ac27e6
javad <jamiri@iosie.net>
2012-09-11 08:51:28 +0430

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Protorails::Application.config.secret_token = '582a32cb2c43bc4456c238b85596ca829054510bfb96e85c05daf5a134c25c199f0b5ef90f0858b0609933d3123e8122122fe58be0e256d0c8b3667d8097b141'
