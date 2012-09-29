d2ef162dcf46d804fff444f34dacd53f724e5bbd
Hussein Samadi <hsamadi@iosie.net>
2012-09-29 12:37:23 +0330

    Glossary controller was implemented.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Protorails::Application.routes.draw do
-  get "home/index"
+  root :to => 'home#index', :as => 'home'
+
+  match '/lesson/:id/terms', :to => 'glossary#lookup_lesson_terms', :as => :lesson_terms
+  match '/glossary/:term', :to => 'glossary#lookup_term', :as => :lookup_term
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +53,6 @@ Protorails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
