269a4520d2c15aa569ad6feaafcd4aba2a23d535
meqdadh <meqdadh@gmail.com>
2012-09-30 14:30:08 +0330

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Protorails::Application.routes.draw do
 
   match '/lesson/:id/terms', :to => 'glossary#lookup_lesson_terms', :as => :lesson_terms
   match '/glossary/:term', :to => 'glossary#lookup_term', :as => :lookup_term
+  match '/lesson/:lesson_title', :to => 'lesson#lesson_show', :as => :lesson_show
 
   match '/lesson/:lesson_id/microblog/create', :to => 'microblog#create', :as => :lookup_term
   match '/lesson/:lesson_id/microblogs/:page', :to => 'microblog#get', :as => :get_micrblog
