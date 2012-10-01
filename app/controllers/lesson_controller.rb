723f343e1d4840d6896c2a389ee1e1a31ecfe962
meqdadh <meqdadh@gmail.com>
2012-10-01 16:37:54 +0330

    Lesson Rating Refactor Finished

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -20,4 +20,26 @@ class LessonController < ApplicationController
 
     @avg_rate = @lesson.lesson_ratings.average("rating")
   end
+  def rate
+
+    unless request.xhr?
+      render :file => "#{Rails.root}/public/404.html", :status => :not_found and return
+    end
+
+    lesson_id = params[:lesson_id]
+    rate_val = params[:rate_val]
+
+    vote = LessonRating.new
+    vote.lesson_id = lesson_id
+    vote.user_id = 1
+    vote.rating = rate_val
+
+    vote.save
+
+
+    respond_to do |format|
+      format.json { render :json => LessonRating.where(:lesson_id => lesson_id).average("rating")   }
+    end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Protorails::Application.routes.draw do
   match '/lesson/:id/terms', :to => 'glossary#lookup_lesson_terms', :as => :lesson_terms
   match '/glossary/:term', :to => 'glossary#lookup_term', :as => :lookup_term
   match '/lesson/:title', :to => 'lesson#show'
+  match '/lesson/:lesson_id/rating/:rate_val', :to => 'lesson#rate'
 
   match '/lesson/:lesson_id/microblog/create', :to => 'microblog#create', :as => :lookup_term
   match '/lesson/:lesson_id/microblogs/:page', :to => 'microblog#get', :as => :get_micrblog
