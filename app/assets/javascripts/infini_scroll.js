5945b43f0bb8c87f6bb27b3e7b179cb7f936cd41
meqdadh <meqdadh@gmail.com>
2012-11-15 14:57:21 +0330

    Infinite Scroll Refactored for waiting algorithm

--- app/assets/javascripts/infini_scroll.js
+++ app/assets/javascripts/infini_scroll.js
@@ -8,25 +8,29 @@ function initInfiniScroll(elem, url, callback) {
 
     elem.page = 1;
     elem.fetching = false;
+    elem.ended = false;
 
     $(window).scroll(function () {
-
         if (elem.is(':visible')) {
             var wintop = $(window).scrollTop(),
                 docheight = $(document).height(),
                 winheight = $(window).height();
 
-            if (((wintop / (docheight - winheight)) > scrolltrigger) && !elem.fetching) {
+            if (((wintop / (docheight - winheight)) > scrolltrigger) && !elem.fetching && !elem.ended) {
 
                 elem.fetching = true;
+
                 $('div#lastPostsLoader').show();
+
                 $.getJSON(url(elem.page), function(data) {
                     callback(data);
                     elem.fetching = false;
                     $('div#lastPostsLoader').hide();
-
                     if ($(data).length > 0) {
                         elem.page += 1;
+                    }
+                    if ($(data).length < 5) {
+                        elem.ended = true;
                     }
                 });
             }
