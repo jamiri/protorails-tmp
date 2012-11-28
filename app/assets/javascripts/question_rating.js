c65107801d6e096b1c35a97f94f205dbb386befd
meqdadh <meqdadh@gmail.com>
2012-11-28 16:04:30 +0330

    show notification for Lesson & Question rating

--- app/assets/javascripts/question_rating.js
+++ app/assets/javascripts/question_rating.js
@@ -7,17 +7,31 @@
  */
 
 function question_vote(question_id, amnt, enabled) {
-    if (enabled && eval("question_"+ question_id +"_not_rated") && user_connected) {
+    if (enabled && eval("question_" + question_id + "_not_rated") && user_connected) {
         $.ajax({
             type:"GET",
             url:"/lesson/" + $('.script:first').attr('lesson_id') + "/question/" + question_id + "/rating/" + amnt,
             dataType:"json",
             success:function (amnt) {
-
                 $("#current-rating-question" + question_id).width(amnt * 25);
-
-            }
+                showNotification({
+                    text:"You have Rate this Question successfully.",
+                    type:"notice"
+                });
+             }
         });
     }
-    eval("question_"+ question_id +"_not_rated = false");
+    if (!user_connected) {
+        showNotification({
+            text:"You are not logged,please Log in before Question Rating.",
+            type:"error"
+        })
+    }
+    else if (!eval("question_" + question_id + "_not_rated")) {
+        showNotification({
+            text:"You already rated this Question.",
+            type:"error"
+        });
+    }
+    eval("question_" + question_id + "_not_rated = false");
 }
